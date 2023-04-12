591049f92ebdd9afc6d8b746c9213c09fefb4cf0
juliana.reitz <juliana.mreitz@gmail.com>
2023-04-12 12:25:53 -0300

    test - adding test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+      1,
+      "CADEIRA",
+      "Forces of Nature",
+      "2022-04-04",
+      "2023-02-09",
+      "FR48",
+      "Conservar em local fresco")
+    assert product.id == 1
+    assert product.nome_do_produto == "CADEIRA"
+    assert product.nome_da_empresa == "Forces of Nature"
+    assert product.data_de_fabricacao == "2022-04-04"
+    assert product.data_de_validade == "2023-02-09"
+    assert product.numero_de_serie == "FR48"
+    assert product.instrucoes_de_armazenamento == "Conservar em local fresco"
