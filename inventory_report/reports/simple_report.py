4b27b1325d5299dca0767ce754f33673f1a8bffa
juliana.reitz <juliana.mreitz@gmail.com>
2023-04-12 14:49:05 -0300

    feat - class SimpleReport (req02)

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,43 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def get_oldest_manufacturing_date(cls, products):
+        return min(product["data_de_fabricacao"] for product in products)
+
+    @classmethod
+    def get_nearest_expiration_date(cls, products):
+        today = datetime.today().date()
+        date_pattern = "%Y-%m-%d"
+        nearest_expiration = min(
+            [
+                product["data_de_validade"]
+                for product in products
+                if datetime.strptime(
+                    product["data_de_validade"], date_pattern
+                ).date()
+                > today
+            ]
+        )
+        return nearest_expiration
+
+    @classmethod
+    def get_company_with_more_products(cls, products):
+        return Counter(
+            product["nome_da_empresa"] for product in products
+        ).most_common()[0][0]
+
+    @classmethod
+    def generate(cls, products):
+        oldest_manufacturing_date = cls.get_oldest_manufacturing_date(products)
+        nearest_expiration_date = cls.get_nearest_expiration_date(products)
+        company_with_more_products = cls.get_company_with_more_products(
+            products
+        )
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {nearest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,13 +3,14 @@ from inventory_report.inventory.product import Product
 
 def test_cria_produto():
     product = Product(
-      1,
-      "CADEIRA",
-      "Forces of Nature",
-      "2022-04-04",
-      "2023-02-09",
-      "FR48",
-      "Conservar em local fresco")
+        1,
+        "CADEIRA",
+        "Forces of Nature",
+        "2022-04-04",
+        "2023-02-09",
+        "FR48",
+        "Conservar em local fresco",
+    )
     assert product.id == 1
     assert product.nome_do_produto == "CADEIRA"
     assert product.nome_da_empresa == "Forces of Nature"
